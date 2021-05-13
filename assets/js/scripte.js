3807c4eaa7ebbeb42db47997ebd9917e8d4544d1
Pfrdl <pierre.fardel19@gmail.com>
2021-05-13 20:10:09 +0200

    ajouts

--- assets/js/scripte.js
+++ assets/js/scripte.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 
 
+
+
+
+
     let playerChoice;
     let iaScore = 0;
     let playerScore = 0;
     let elements = ['pierre', 'feuille', 'ciseaux'];
     let game = 0
-
 
 
     $('button').click(function() {
@@ -52,136 +55,133 @@ $(document).ready(function() {
         let percentPlayer = ((playerScore / game) * 100)
         $('#playerPercentCtn').html(Math.round(percentPlayer) + '%')
         $('#iaPercentCtn').html(Math.round(percentIa) + '%')
-        $('#playerScore').text(playerScore);
-        $('#iaScore').text(iaScore)
+        let affichageplayer = $('#playerScore').text(playerScore);
+        let affichageIa = $('#iaScore').text(iaScore)
 
 
 
+
+
+        $('#restart').click(function() {
+
+
+            location.reload()
+
+
+
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // PIERRE
+        // $('#playerStoneCtn').click(function() {
+        //     let stone = $('.stone').hide();
+        //     let paper = $('.paper').hide();
+        //     let scissor = $('.scissor').hide();
+
+        //     playerChoice = 0;
+        //     let randomElement = Math.floor(Math.random() * 3);
+        //     if (randomElement == 0) {
+
+        //         $('#partieBloc').html('Egalité')
+        //         stone.show();
+
+        //     } else if (randomElement == 1) {
+        //         $('#partieBloc').html('Perdu');
+        //         paper.show();
+        //         iaScore.html(1);
+
+        //     } else {
+        //         $('#partieBloc').html('Gagné !')
+        //         scissor.show();
+
+
+        //     }
+
+        // })
+
+
+
+        // // FEUILLE
+        // $('#playerPaperCtn').click(function() {
+        //     let stone = $('.stone').hide();
+        //     let paper = $('.paper').hide();
+        //     let scissor = $('.scissor').hide();
+
+        //     playerChoice = 1;
+        //     let randomElement = Math.floor(Math.random() * 3);
+
+        //     if (randomElement == 1) {
+        //         $('#partieBloc').html('Egalité')
+        //         paper.show();
+
+        //     } else if (randomElement == 2) {
+        //         $('#partieBloc').html('Perdu')
+        //         scissor.show();
+
+        //     } else {
+        //         $('#partieBloc').html('Gagné !')
+        //         stone.show();
+        //     }
+        // })
+
+
+        // // CISEAUX
+        // $('#playerScissorCtn').click(function() {
+        //     let stone = $('.stone').hide();
+        //     let paper = $('.paper').hide();
+        //     let scissor = $('.scissor').hide();
+        //     playerChoice = 2;
+        //     let randomElement = Math.floor(Math.random() * 3);
+
+        //     if (randomElement == 2) {
+        //         $('#partieBloc').html('Egalité')
+        //         scissor.show();
+
+        //     } else if (randomElement == 0) {
+        //         $('#partieBloc').html('Perdu')
+        //         stone.show();
+
+        //     } else {
+        //         $('#partieBloc').html('Gagné !')
+        //         paper.show();
+        //     }
+
+        // })
+
+
+
+        // $('#restart').click(function() {
+
+
+        //     iaScore.empty();
+        //     playerScore.empty()
+
+
+
+
+
+
+        // })
 
 
 
     })
-
-
-
-
-
-    $('#restart').click(function() {
-
-
-        $('button').trigger("reset");
-        return false;
-
-
-
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // PIERRE
-    // $('#playerStoneCtn').click(function() {
-    //     let stone = $('.stone').hide();
-    //     let paper = $('.paper').hide();
-    //     let scissor = $('.scissor').hide();
-
-    //     playerChoice = 0;
-    //     let randomElement = Math.floor(Math.random() * 3);
-    //     if (randomElement == 0) {
-
-    //         $('#partieBloc').html('Egalité')
-    //         stone.show();
-
-    //     } else if (randomElement == 1) {
-    //         $('#partieBloc').html('Perdu');
-    //         paper.show();
-    //         iaScore.html(1);
-
-    //     } else {
-    //         $('#partieBloc').html('Gagné !')
-    //         scissor.show();
-
-
-    //     }
-
-    // })
-
-
-
-    // // FEUILLE
-    // $('#playerPaperCtn').click(function() {
-    //     let stone = $('.stone').hide();
-    //     let paper = $('.paper').hide();
-    //     let scissor = $('.scissor').hide();
-
-    //     playerChoice = 1;
-    //     let randomElement = Math.floor(Math.random() * 3);
-
-    //     if (randomElement == 1) {
-    //         $('#partieBloc').html('Egalité')
-    //         paper.show();
-
-    //     } else if (randomElement == 2) {
-    //         $('#partieBloc').html('Perdu')
-    //         scissor.show();
-
-    //     } else {
-    //         $('#partieBloc').html('Gagné !')
-    //         stone.show();
-    //     }
-    // })
-
-
-    // // CISEAUX
-    // $('#playerScissorCtn').click(function() {
-    //     let stone = $('.stone').hide();
-    //     let paper = $('.paper').hide();
-    //     let scissor = $('.scissor').hide();
-    //     playerChoice = 2;
-    //     let randomElement = Math.floor(Math.random() * 3);
-
-    //     if (randomElement == 2) {
-    //         $('#partieBloc').html('Egalité')
-    //         scissor.show();
-
-    //     } else if (randomElement == 0) {
-    //         $('#partieBloc').html('Perdu')
-    //         stone.show();
-
-    //     } else {
-    //         $('#partieBloc').html('Gagné !')
-    //         paper.show();
-    //     }
-
-    // })
-
-
-
-    // $('#restart').click(function() {
-
-
-    //     iaScore.empty();
-    //     playerScore.empty()
-
-
-
-
-
-
-    // })
-
-
 
 })
